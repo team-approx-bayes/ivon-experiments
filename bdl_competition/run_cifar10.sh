00a0cd01ac4544ce55b39609a46b29447a06b409
Thomas <thomas.moellenhoff@tum.de>
2024-06-11 17:19:19 +0900

    cifar10 competition

--- bdl_competition/run_cifar10.sh
+++ bdl_competition/run_cifar10.sh
@@ -7,14 +7,15 @@ beta2=0.9999
 hess_init=0.005
 epochs=120
 bs=50 
-ess=200000
+ess=200000.0
 wd=0.0002
 mc=1
 save_dir=./results_cifar10/
 
 for seed in {1..6}; do
     python train.py cifar10 --learning_rate ${lr} --save_dir ${save_dir} --seed ${seed} \
-        --epochs ${epochs} --hess_init ${hess_init} --tbatch ${bs} --momentum_hess ${beta2} --momentum ${beta1} \
+        --epochs ${epochs} --hess_init ${hess_init} --tbatch ${bs} --momentum_hess ${beta2} \
+        --momentum ${beta1} --lr_final ${lr_final} --warmup 0 \
         --ess ${ess} --wd ${wd} --mc_samples ${mc} --device cuda
 done 
 
